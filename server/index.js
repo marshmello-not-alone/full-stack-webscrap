45727ec3b7ec276aa6fba0fdee7b10060e570bd7
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-01-23 15:39:47 -0500

    ✨ Setting up express server

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+//custom middle to disable security rules for local development
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
+
+app.get('/creators', async(req, res) => {
+  const creators = [
+    {name: "omer", img: 'https://'},
+    {name: "jiaqi", img: 'https://'},
+    {name: "dylan", img: 'https://'},
+  ]
+  //todo: GET from DB
+  res.send(creators)
+})
+
+app.post('/creators', async(req, res) => {
+  console.log(req.body)
+  // todo: Scrape channel
+  // todo: Add to DB
+  res.send('success')
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
